0e4908b7f73e8c9b3f9fdbf939956af1af075397
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-01-26 15:14:06 +0530

    Learned About Routing through pages, passing data to view, redirect pages and many other routing methods

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<h1>hello this is the home file</h1>
+
+<a href="/about/kavinda">About Kavinda</a>
+<a href="/about/nimesh">About Nimesh</a>
+<a href="/">Welcome Page</a> // This is the link to the home page 
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// usual method for routing
+Route::get('/home', function(){
+    return view('home');
+});
+
+Route::view('/home','home'); 
+// another method to routing
+
+// passing data to view
+Route::get('/about/{name}', function($name){
+    return view('about',['name'=>$name]);
+});
+
+// this is how to rederect a page..this call redirect home page to root page
+Route::redirect('/home', '/');
