0072c9864fe265dd3c9393ec6191ccd0a0c5fe01
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-01-27 23:53:21 +0530

    Learned About components

---
+++ resources/views/components/messagebanner.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <span class= "{{$class}}"> {{$msg}}</span>
+    <!-- The only way to do great work is to love what you do. - Steve Jobs -->
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,39 @@
-@include('common.header')
 
+<x-message-banner msg="User added sucsesfully" class="succsess"/>
+<x-message-banner msg="User logout sucsesfully" class="fail"/>
+<!-- Include the message banner component -->
 <h1>Home Page</h1>
 
-@include('common.inner', ["page" => "This is Home page"])
+
+
+<style>
+    .succsess{
+        color: green;
+        font-weight: 800;
+        padding: 10px;
+        border-radius: 10px;
+        width: 50%;
+        margin: 50px;
+        gap: 10px;
+        background-color: lightgreen;
+        display: flex;
+        text-align: center;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .fail{
+        color: red;
+        font-weight: 800;
+        padding: 10px;
+        border-radius: 10px;
+        width: 50%;
+        margin: 50px;
+        gap: 10px;
+        background-color: lightpink;
+        display: flex;
+        text-align: center;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
