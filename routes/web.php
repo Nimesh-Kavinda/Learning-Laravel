83bce7bbeeb4a9ab8a3d85c0491dbb7d75b6ba0a
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-01-27 13:23:07 +0530

    Learned about View with routing contoller and pass data and aslo checking if it exixts or not

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController; // Import the controller
+use App\Http\Controllers\UserController;// Import the controller
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::view('home', 'home');
+Route::view('about', 'about');
+
+Route::get('user-home', [UserController::class, 'userHome']);
+Route::get('user-about/{name}', [UserController::class, 'userAbout']);
+
+Route::view('admin', 'admin.login');
+
+Route::get('admin-login', [UserController::class, 'adminLogin']);
