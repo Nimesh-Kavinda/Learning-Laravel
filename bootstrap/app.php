8cf02ad77d4dadcf91ba567ee0e826acbfb91e77
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-01-30 23:35:22 +0530

    Learne about the middelware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\age;
+use Illuminate\Support\Facades\App;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        // $middleware->append(age::class); // This is the same as the snippet below
+        $middleware->use([                 
+            \App\Http\Middleware\age::class,        
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>home Page</h1>
+</div>
