3d63845d4dd2a01447bf1855552732c9d8f3e025
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-01-31 00:15:24 +0530

    Delte some preivious files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,12 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
-        $middleware->appendToGroup('check1',[
-            Countrycheck::class,
-            Agecheck::class
-        ]
-            
-        );
        
     })
     ->withExceptions(function (Exceptions $exceptions) {
--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('home', 'home')->middleware('check1');
-// Route::view('about', 'about')->middleware('check1');
+Route::view('home', 'home');
 
 
-Route::middleware('check1')->group(function(){
-    Route::view('about', 'about');
-    Route::view('list', 'about');
-    Route::view('contact', 'about');
-    Route::view('user', 'about');
-});
 
-// Route::view('about', 'about');
-// Route::view('list', 'about');
-// Route::view('contact', 'about');
-// Route::view('user', 'about');
+
+
