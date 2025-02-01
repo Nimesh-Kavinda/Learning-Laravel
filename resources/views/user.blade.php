91c9c74e1143136e699dd4c5ea8a9387a6e21054
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-02-01 10:49:00 +0530

    Learn about the HTTP Clients

---
+++ resources/views/user.blade.php
@@ -0,0 +1,24 @@
+<div>
+    <h1>User data</h1>  
+    
+        <ul>
+            <li>
+                <span>Name : </span>
+                <span><b>{{$data->name}}</b></span>
+            </li>
+            <li>
+                <span>EMail : </span>
+                <span><b>{{$data->email}}</b></span>
+            </li>
+            <li>
+                <span>Phone : </span>
+                <span><b>{{$data->phone}}</b></span>
+            </li>
+            <li>
+                <span>Website : </span>
+                <span><b>{{$data->website}}</b></span>
+            </li>
+        </ul>
+
+<!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentController;
+use App\Http\Controllers\UserController;
 
 
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('user', [UserController::class, 'getData']);
 
 
 
