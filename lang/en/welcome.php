2f2afcbadd60cd9784454aee372d7575b444bf93
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-02-03 01:33:25 +0530

    Learn about the localization (part 01)

---
+++ lang/en/welcome.php
@@ -0,0 +1,13 @@
+<?php 
+
+return [
+    "welcome"=> "Hi, How Are You",
+    "heading"=> "Welcome to the Home Page",
+    "about"=> "About",
+    "home"=> "Home",
+    "contact"=> "Conatct us",
+]
+
+
+
+?>
