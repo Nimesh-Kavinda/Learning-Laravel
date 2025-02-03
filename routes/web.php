f8bfc3ea767a329f5e84fdaed31eba70be5973dd
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-02-03 17:36:00 +0530

    Clear previous Data and files

--- routes/web.php
+++ routes/web.php
@@ -9,27 +9,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('add', 'add-student');
 
 
-Route::post('add-student',[StudentController::class, 'add']);
-Route::get('list-student',[StudentController::class, 'list']);
-Route::get('delete/{id}',[StudentController::class, 'delete']);
-Route::get('edit/{id}',[StudentController::class, 'edit']);
-
-Route::put('edit-student/{id}',[StudentController::class, 'editStudent']);
-
-Route::get('search', [StudentController::class, 'search']);
-
-Route::post('delete-multi', [StudentController::class, 'deleteMultiple']);
-
-
-Route::view('upload', 'upload');
-Route::view('display', 'display');
-
-Route::post('upload', [ImageController::class, 'upload']);
-
-Route::get('img-list', [ImageController::class, ('list')]);
 
 
 
