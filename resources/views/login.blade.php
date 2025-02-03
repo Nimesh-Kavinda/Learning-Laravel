b840f6aae737671954c3ffd85cd2e1cec9626ce3
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-02-03 19:09:17 +0530

    Learn Howt to add Bosstrap in blade

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,11 +5,19 @@
         <h1>Login Form</h1>
         <form action="" method="post">
             @csrf 
-            <input type="text" name="username" placeholder="Enter username">
-            <br><br>
-            <input type="password" name="password" placeholder="Enter Password">
-            <br><br>
-            <button type="submit">Login</button>
+            <div class="mb-3 row">
+    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+    <div class="col-sm-10">
+      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+    </div>
+  </div>
+  <div class="mb-3 row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword">
+    </div>
+  </div>
+            <button type="submit" class="btn btn-primary">Login</button>
         </form>
     </x-slot>
 </x-layout>
