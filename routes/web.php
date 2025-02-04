c8ff52040f56e6d0ae3e10a4ed0c4fc15378e69b
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-02-04 17:28:26 +0530

    clear previous data in web.php

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,7 @@ Route::get('/', function () {
 }); 
 
 
-$data = "hi, lets's learn laravel";
 
-// $data = Str::ucfirst($data);
-// $data = Str::replaceFirst("Hi", "Hello", $data);
-// $data = Str::camel($data);
-
-$data = Str::of($data)
-                ->ucfirst($data)
-                ->replaceFirst("Hi", "Hello", $data);
-                
-
-echo $data;
 
 
 
